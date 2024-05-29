c2cb8f64111e6ddffe5a1f4e5e51275d389e2ec7
Jonathan Boada <jonathanboada1@gmail.com>
2024-05-29 16:58:18 -0500

    Validaciones de registro mail

---
+++ views/auth/mensaje.php
@@ -0,0 +1,4 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Es necesario confirmar tu cuenta, revisa tu banedja de entrada para hacerlo</p>
+</main>
--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,20 +2,27 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Registarte en DevWebCamp</p>
 
-    <form action="" class="formulario">
+    <?php
+        require_once __DIR__ . '/../templates/alertas.php';    
+    ?>
+
+    <form action="/registro" method="post" class="formulario">
         <div class="formulario__campo">
             <label for="nombre" class="formulario__label">Nombre</label>
-            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre">
+            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre"
+                value="<?php echo $usuario->nombre; ?>">
         </div>
 
         <div class="formulario__campo">
             <label for="apellido" class="formulario__label">Apellido</label>
-            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido">
+            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido"
+                value="<?php echo $usuario->apellido; ?>">
         </div>
 
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
-            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email">
+            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email"
+                value="<?php echo $usuario->email; ?>">
         </div>
 
         <div class="formulario__campo">
