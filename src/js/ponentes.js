d37b87cd8a65d8e9f507aac92a7ca1f6bd751834
Jonathan Boada <jonathanboada1@gmail.com>
2024-06-06 22:39:23 -0500

    correciones script ponentes.js

--- src/js/ponentes.js
+++ src/js/ponentes.js
@@ -11,12 +11,12 @@
         async function obtenerPonentes() {
             const url = '/api/ponentes';
             const respuesta = await fetch(url);
-            resultado = await respuesta.json();
+            const resultado = await respuesta.json();
             formatearPonentes(resultado);
         }
 
         function formatearPonentes(arrayPonentes = []) {
-            ponentes = arrayPonentes.map(ponente =>{
+            ponentes = arrayPonentes.map(ponente => {
                 return {
                     nombre: `${ponente.nombre.trim()} ${ponente.apellido.trim()}`,
                     id: ponente.id
@@ -25,11 +25,17 @@
         }
 
         function buscarPonentes(e) {
-           const busqueda = e.target.value;
-           if(busqueda.lenght > 3) {
-            //Expresion regular para buscar coincidencias
-            const expresion = new RegExp(busqueda, 'i');
-           }
+            const busqueda = e.target.value;
+            if (busqueda.length > 3) {
+                //Expresion regular para buscar coincidencias
+                const expresion = new RegExp(busqueda, "i");
+                ponentesFiltrados = ponentes.filter(ponente => {
+                    if (ponente.nombre.toLowerCase().search(expresion) != -1) {
+                        return ponente;
+                    }
+                    })
+                console.log(ponentesFiltrados);
+            }
         }
     }
 })();
