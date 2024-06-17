b3eb22133725a0a63956326bd9e456acf8a7f2b2
Jonathan Boada <jonathanboada1@gmail.com>
2024-06-17 09:55:44 -0500

    Añadiendo Curresel a las demas secciones

--- views/paginas/conferencias.php
+++ views/paginas/conferencias.php
@@ -8,29 +8,29 @@
         <div class="eventos__listado slider swiper">
             <div class="swiper-wrapper">
                 <?php foreach ($eventos['conferencias_v'] as $evento) { ?>
-                <div class="evento swiper-slide">
-                    <p class="evento__hora"> <?php echo $evento->hora->hora; ?></p>
-                    <div class="evento__informacion">
-                        <h4 class="evento__nombre"> <?php echo $evento->nombre; ?></h4>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"> <?php echo $evento->hora->hora; ?></p>
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"> <?php echo $evento->nombre; ?></h4>
 
-                        <p class="evento__introduccion"> <?php echo $evento->descripcion; ?></p>
+                            <p class="evento__introduccion"> <?php echo $evento->descripcion; ?></p>
 
-                        <div class="evento__autor-info">
-                            <picture>
-                                <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp"
-                                    type="image/webp">
-                                <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png"
-                                    type="image/png">
-                                <img class="evento__imagen-autor" loading="lazy" width="200" height="300"
-                                    src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
-                            </picture>
+                            <div class="evento__autor-info">
+                                <picture>
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp"
+                                        type="image/webp">
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png"
+                                        type="image/png">
+                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300"
+                                        src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                                </picture>
 
-                            <div class="evento__autor-nombre">
-                                <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                <div class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 <?php } ?>
             </div>
             <div class="swiper-button-next"></div>
@@ -38,15 +38,105 @@
         </div>
 
         <p class="eventos__fechas">Sábado 6 de Octubre</p>
-        <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['conferencias_s'] as $evento) { ?>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"> <?php echo $evento->hora->hora; ?></p>
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"> <?php echo $evento->nombre; ?></h4>
+
+                            <p class="evento__introduccion"> <?php echo $evento->descripcion; ?></p>
+
+                            <div class="evento__autor-info">
+                                <picture>
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp"
+                                        type="image/webp">
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png"
+                                        type="image/png">
+                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300"
+                                        src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                                </picture>
+
+                                <div class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
     </div>
 
     <div class="eventos eventos--workshops">
         <h3 class="eventos__heading">&lt;Workshops /></h3>
         <p class="eventos__fechas">Viernes 5 de Octubre</p>
-        <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['workshops_v'] as $evento) { ?>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"> <?php echo $evento->hora->hora; ?></p>
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"> <?php echo $evento->nombre; ?></h4>
+
+                            <p class="evento__introduccion"> <?php echo $evento->descripcion; ?></p>
+
+                            <div class="evento__autor-info">
+                                <picture>
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp"
+                                        type="image/webp">
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png"
+                                        type="image/png">
+                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300"
+                                        src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                                </picture>
+
+                                <div class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
 
         <p class="eventos__fechas">Sábado 6 de Octubre</p>
-        <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['workshops_s'] as $evento) { ?>
+                    <div class="evento swiper-slide">
+                        <p class="evento__hora"> <?php echo $evento->hora->hora; ?></p>
+                        <div class="evento__informacion">
+                            <h4 class="evento__nombre"> <?php echo $evento->nombre; ?></h4>
+
+                            <p class="evento__introduccion"> <?php echo $evento->descripcion; ?></p>
+
+                            <div class="evento__autor-info">
+                                <picture>
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.webp"
+                                        type="image/webp">
+                                    <source srcset="img/speakers/<?php echo $evento->ponente->imagen; ?>.png"
+                                        type="image/png">
+                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300"
+                                        src="img/speakers/<?php echo $evento->ponente->imagen; ?>.png" alt="Imagen Ponente">
+                                </picture>
+
+                                <div class="evento__autor-nombre">
+                                    <?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
     </div>
 </main>
